409bb825f88ad13528280f855015815382e9f84a
hudsontheuz <hudsonknad@gmail.com>
2025-06-03 21:29:04 -0300

    pages

---
+++ src/components/UserList.tsx
@@ -0,0 +1,22 @@
+import { List, ListItem, ListItemText } from "@mui/material";
+import type { User } from "../types/User";
+import React from "react";
+
+interface Props {
+  users: User[];
+}
+
+export default function UserList({ users }: Props) {
+  return (
+    <List>
+      {users.map((user) => (
+        <ListItem key={user.id}>
+          <ListItemText
+            primary={user.nome}
+            secondary={`${user.email} - ${user.telefone}`}
+          />
+        </ListItem>
+      ))}
+    </List>
+  );
+}
